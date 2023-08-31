03d844d30d4fc3df13196863c51af5ebd63443dd
Squidgi <stephanileonardi@gmail.com>
2023-08-31 14:38:16 +1000

    Building a discord bot

---
+++ bot.py
@@ -0,0 +1,39 @@
+import discord
+import responses
+import config
+
+async def send_message(message, user_message, is_private):
+    try:
+        response = responses.handle_response(user_message)
+        await message.channel.send(response) if is_private else await message.channel.send(response)
+    except Exception as e:
+        print(e)
+
+
+def run_discord_bot():
+    TOKEN = config.TOKEN
+    intents = discord.Intents.all()
+    client = discord.Client(intents=intents)
+
+
+    @client.event
+    async def on_ready():
+        print(f'{client.user} is now running!')
+
+
+
+    @client.event
+    async def on_message(message):
+        # prevents bot from responding to itself
+        if message.author == client.user:
+            return
+
+        username = str(message.author)
+        user_message = str(message.content)
+        channel = str(message.channel)
+
+        print(f"{username} said: '{user_message}' ({channel})")
+
+
+
+    client.run(TOKEN)
